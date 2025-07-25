84e19f0bff7dbda1643b2be5b4658549db71d85d
imtangx <939174106@qq.com>
2025-07-25 22:30:10 +0800

    feat(lang): 语言包搭建初始化

---
+++ packages/codemirror-lang/tsup.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  splitting: false,
+  sourcemap: true,
+  minify: false,
+  target: 'es2020',
+  outDir: 'dist',
+  external: [
+    '@markdown-editor/shared',
+    '@markdown-editor/lexer',
+    '@markdown-editor/ast',
+    '@markdown-editor/parser',
+    '@markdown-editor/renderer',
+  ],
+  tsconfig: './tsconfig.json',
+});
